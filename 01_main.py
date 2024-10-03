ffa4a20087ab50753c67e946df19c72f26172576
TYJ02 <yujing2788@gmail.com>
2024-10-03 20:06:33 +0800

    final commit for demo

--- 01_main.py
+++ 01_main.py
@@ -11,7 +11,7 @@ st.title("Container Inspection Dashboard")
 st.sidebar.success("Select a page above.")
 
 # load sql database
-con = sqlite3.connect('database.db')
+con = sqlite3.connect('database-demo.db')
 cur = con.cursor()
 res = cur.execute("SELECT * FROM Containers")
 container = res.fetchall()
@@ -21,7 +21,7 @@ res = cur.execute("SELECT * FROM Inspection")
 time = res.fetchall()
 con.close()
 damagedf = pd.DataFrame(damage, columns =["damage_id", "container_id", "timestamp", "damage_type", "location"])
-timedf = pd.DataFrame(time, columns =["inspect_id", "container_id", "timestamp", "img_path"])
+timedf = pd.DataFrame(time, columns =["inspect_id", "container_id", "timestamp", "img_path", "grade"])
 
 option = ''
 path = ''
@@ -73,11 +73,52 @@ def inspect_date(option):
     st.write(time)
     return time
 
-def container_grade(option, timestamp)
-    grade = timedf['grade'][(timedf['container_id']==option) & (timedf['timestamp'] == timestamp)].iloc[0]
-    st.metric(label="grade", value = grade)
-    #st.markdown(f" ### timestamp \n this is {timestamp}")
+def container_grade(option, timestamp):
+    try:
+        grade = timedf[['grade']][(timedf['container_id']==option) & (timedf['timestamp'] == timestamp)].iloc[0]
+    except IndexError:
+        grade = 'None'
+    st.header("Container Grade")
+    st.metric(label="", value = grade)
 
+
+def damage_history():
+    print('hi')
+
+
+def export_pdf(id, time):
+    if st.button("export report"):
+        df = damagedf[(damagedf['container_id']==option) & (damagedf['timestamp'] == timestamp)]
+        createpdf.report(id, time, df)
+        print('pdf exported')
+
+
+def damage_count(option):
+    count = df["count"][df["id"] == option].iloc[0]
+    st.header("damage count")
+    st.write(count)
+
+
+def damage_type(option):
+    damages = df[["axis","concave","dentado","perforation"]][df["id"] == option].iloc[0]
+    st.header("damage type")
+    col3, col4, col5, col6 = st.columns(4)
+    label = ["axis", "concave", "dentado", "perforation"]
+    col3.metric(label=label[0], value = damages[0])
+    col4.metric(label=label[1], value = damages[1])
+    col5.metric(label=label[2], value = damages[2])
+    col6.metric(label=label[3], value = damages[3])
+
+
+col1, col2  = st.columns(2)
+with col1:
+    option, path = container_id()
+    timestamp = inspect_date(option)
+    export_pdf(option, timestamp)
+with col2:
+    container_image(option, path)
+    container_grade(option, timestamp)
+    #st.metric(label="current", value = option)
 
 damage = damagedf[(damagedf['container_id']==option) & (damagedf['timestamp'] == timestamp)]
 # interactive image - click to zoom
