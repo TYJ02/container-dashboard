2791fb2da445c21dd3484866588d0a3664a0376b
TYJ02 <yujing2788@gmail.com>
2024-10-03 17:10:49 +0800

    add iso column for Containers table

--- create-database.py
+++ create-database.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-con = sqlite3.connect('database.db')
+con = sqlite3.connect('database-demo.db')
 cur = con.cursor()
 
 
 def create_table():
 # when writing in python sqlite3, we omit semicolon
-    cur.execute("CREATE TABLE Containers ( container_id VARCHAR(50) PRIMARY KEY );")
+    cur.execute("CREATE TABLE Containers ( container_id VARCHAR(50) PRIMARY KEY, iso TEXT NOT NULL );")
     cur.execute("CREATE TABLE Damages ( damage_id INTEGER PRIMARY KEY AUTOINCREMENT, container_id VARCHAR(50), timestamp TEXT NOT NULL, damage_type VARCHAR(100), location TEXT NOT NULL, FOREIGN KEY (container_id) REFERENCES Containers(container_id));")
     cur.execute("CREATE TABLE Inspection ( inspect_id INTEGER PRIMARY KEY AUTOINCREMENT, container_id VARCHAR(50), timestamp TEXT NOT NULL, img_path TEXT NOT NULL, FOREIGN KEY (container_id) REFERENCES Containers(container_id));")
     con.commit()
@@ -20,6 +20,9 @@ FOREIGN KEY (container_id) REFERENCES Containers(container_id)
 );
 '''
 
+def add_id_demo(data):
+    cur.executemany("INSERT INTO Containers (container_id, iso) VALUES (?,?)", data)
+    con.commit()
 
 def add_id():
     cur.execute("INSERT INTO Containers (container_id) VALUES ('IAAU1718635'),('AXEU6000191'),('BHCU5000265'),('NLLU4223960'),('OOCU7342259'),('TCNU6215064'),('TGBU7713246');")
@@ -35,5 +38,6 @@ def add_inspect(data):
     con.commit()
 
 if __name__ == "__main__":
-    create_table()
-    add_id()
+    data = [("TCNU6215064", "45G1"), ("TCNU2558065", "45G1")]
+    #create_table()
+    add_id_demo(data)
